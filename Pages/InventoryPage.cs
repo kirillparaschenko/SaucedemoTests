1489952784cd0b7cf818c68bc3385e55caa2cd65
kirillparaschenko <kparaschenko@caeonline.com>
2023-05-18 18:33:50 +0300

    Added LoginPage, InventoryPage, LoginPage,  User class, Added methods to BaseTest

---
+++ Pages/InventoryPage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Pages
+{
+    public class InventoryPage : BasePage
+    {
+        public InventoryPage(WebDriver driver) : base(driver)
+        {
+        }
+    }
+}
--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -1,10 +1,29 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Tests.Pages;
+
 namespace Tests.Tests
 {
     public class Tests
     {
+        protected WebDriver ChromeDriver { get; set; }
+        public LoginPage LoginPage { get; set; }
+
         [SetUp]
         public void Setup()
         {
+            ChromeDriver = new ChromeDriver();
+            ChromeDriver.Manage().Window.Maximize();
+            ChromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            LoginPage = new LoginPage(ChromeDriver);
+
+            ChromeDriver.Navigate().GoToUrl("https://www.sharelane.com/cgi-bin/main.py");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            ChromeDriver.Quit();
         }
 
     }
